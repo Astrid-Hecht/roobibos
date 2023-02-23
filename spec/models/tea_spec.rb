f76039edd7bf7d9d354942fa8860874119019d0f
Astrid Hecht <silentaviary@gmail.com>
2023-02-22 20:26:39 -0700

    Test: Fix incorrect tea validations

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Tea, type: :model do
     it { should have_many(:subscriptions).through(:subscription_teas) }
   end
   describe "validations" do
-    it { should validate_presence_of(:first_name) }
-    it { should validate_presence_of(:last_name) }
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:temperature) }
+    it { should validate_presence_of(:brew_time) }
   end
 end
