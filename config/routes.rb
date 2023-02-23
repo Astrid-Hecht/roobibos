d3e12f0cf3cbae714bf0f80a906a5f5227511f54
Astrid Hecht <silentaviary@gmail.com>
2023-02-22 20:42:34 -0700

    Write draft routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :subscription_teas
-  resources :subscriptions
-  resources :customers
-  resources :teas
+  namespace :api do
+    namespace :v1 do
+      resources :customers, only: [:show]
+      resources :subscriptions, only: [:post, :patch, :show]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
