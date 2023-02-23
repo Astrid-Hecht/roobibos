14c8b2a16bc5660744fe2386abce79d465c510ce
Astrid Hecht <silentaviary@gmail.com>
2023-02-22 21:23:14 -0700

    Test: Rewrite scaffold tests to fit usecase

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :customer_subscriptions
   has_many :subscriptions, through: :customer_subscriptions
   validates :first_name, presence: true
   validates :last_name, presence: true
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,6 +5,7 @@ class Subscription < ApplicationRecord
   validates :status, presence: true
   validates :frequency, presence: true
 
+  has_many :customer_subscriptions
   has_many :customers, through: :customer_subscriptions
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
